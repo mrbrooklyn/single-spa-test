e25da4deafef8370772393a6e7431f151c364bb0
Damrongdech Choekpanitsiri <damrongdech@a-host.co.th>
2022-06-06 22:01:13 +0700

    First commit Message

---
+++ angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '**', component: EmptyRouteComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
