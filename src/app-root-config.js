660a2af00fc41ef73a79f365ffb81245b3679f7d
Brook <damrongdech@a-host.co.th>
2022-06-09 11:11:29 +0700

    fixed webpack

--- src/app-root-config.js
+++ src/app-root-config.js
@@ -1,20 +1,26 @@
 import { registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
+import * as isActive from "./activity-functions";
 
-applications.forEach(registerApplication);
-layoutEngine.activate();
+registerApplication(
+  "@thawkin3/single-spa-demo-nav",
+  () => System.import("@thawkin3/single-spa-demo-nav"),
+  isActive.nav,
+  { domElement: document.getElementById("nav-container") }
+);
+
+registerApplication(
+  "@thawkin3/single-spa-demo-page-1",
+  () => System.import("@thawkin3/single-spa-demo-page-1"),
+  isActive.page1,
+  { domElement: document.getElementById("page-1-container") }
+);
+
+registerApplication(
+  "@thawkin3/single-spa-demo-page-2",
+  () => System.import("@thawkin3/single-spa-demo-page-2"),
+  isActive.page2,
+  { domElement: document.getElementById("page-2-container") }
+);
+
 start();
